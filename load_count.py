bc45cb3429b9d9bca4b083a2bdec9cb6e224b1c4
tw1st0fFate <95728344+tw1st0fFate@users.noreply.github.com>
2021-12-12 16:13:35 +0100

    Add files via upload
    New version

--- load_count.py
+++ load_count.py
@@ -1,10 +1,20 @@
 from flask import Flask
 app = Flask(__name__)
 
-count = 0
+file = open('cuvar.txt', 'r')
+count = int(file.read())
+file.close()
 
 @app.route('/')
 def main():
     global count
     count += 1
-    return 'I have been viewed ' + str(count) + ' times.'
+    
+    file = open('cuvar.txt', 'w+')
+    file.write(str(count))
+    file.close()
+
+    return 'I have been viewed ' + str(count) + '. times.'
+
+
+
